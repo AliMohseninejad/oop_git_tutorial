acbfb2dbb8e1a024959b7092ee90f2d7ec29d82f
AliMohseninejad <alimohseninejad@gmail.com>
2025-03-06 19:11:51 +0330

    init: Add main.cpp file

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int sum(int first, int second){
+    int x = first + second;
+    return x;
+}
+
+int main() {
+
+    int number;
+    int value = sum(12, 13);
+    int value2 = sum(-1, 0);
+    return 0;
+}
